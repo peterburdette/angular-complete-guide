a7b9cdba33f06c6f1ccce5c9c09f7e84a6b82fff
Peter Burdette <peterburdette@gmail.com>
2021-12-29 16:53:24 -0500

    created an alias for a custom property

--- 04-components-and-databinding/src/app/server-element/server-element.component.ts
+++ 04-components-and-databinding/src/app/server-element/server-element.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ServerElementComponent implements OnInit {
   // using typescript to define the type, name and content that will get passed.
   // @Input() is a decorator function that allows the custom selector 'element' to be binded to in the child component (it essentially allows for the data that is passed to be received)
-  @Input() element: { type: string; name: string; content: string };
+  // 'srvElement' is the property from the parent component that gets passed into @Input() which will create an alias. The 'element' can be used within the component instead of 'srvElement'.
+  @Input('srvElement') element: { type: string; name: string; content: string };
 
   constructor() {}
 
