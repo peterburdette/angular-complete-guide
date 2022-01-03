5d4a179d535cec2b4a175702909054db335cd26b
Peter Burdette <peterburdette@gmail.com>
2022-01-03 11:25:04 -0500

    used the lifecycle hook ngDoCheck

--- 04-components-and-databinding/src/app/server-element/server-element.component.ts
+++ 04-components-and-databinding/src/app/server-element/server-element.component.ts
@@ -4,6 +4,7 @@ import {
   Input,
   OnChanges,
   SimpleChanges,
+  DoCheck,
 } from '@angular/core';
 
 @Component({
@@ -13,7 +14,7 @@ import {
 })
 
 // it's good practice to include the lifecycle hooks that are implemented
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck {
   // using typescript to define the type, name and content that will get passed.
   // @Input() is a decorator function that allows the custom selector 'element' to be binded to in the child component (it essentially allows for the data that is passed to be received)
   // 'srvElement' is the property from the parent component that gets passed into @Input() which will create an alias. The 'element' can be used within the component instead of 'srvElement'.
@@ -24,6 +25,7 @@ export class ServerElementComponent implements OnInit, OnChanges {
     console.log('constructor called');
   }
 
+  // this is executed when any data-bound property of a directive changes
   ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnChanges called');
     console.log(changes);
@@ -31,5 +33,10 @@ export class ServerElementComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     console.log('ngOnInit called');
+  }
+
+  // this is executed on every change detection run
+  ngDoCheck() {
+    console.log('ngDoCheck called');
   }
 }
