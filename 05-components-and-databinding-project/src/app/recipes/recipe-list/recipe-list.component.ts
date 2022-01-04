452fca55ff379eede32653ed357c36f14adf26f2
Peter Burdette <peterburdette@gmail.com>
2022-01-04 10:52:44 -0500

    initial commit

---
+++ 05-components-and-databinding-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  // the 'Recipe[]' indicates that we are using our model which contains the types of properties that will be used within this array.
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
