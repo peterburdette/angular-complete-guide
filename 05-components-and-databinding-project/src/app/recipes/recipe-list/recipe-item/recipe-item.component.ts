6e21122767c426116fa7fccad62f2175bec78465
Peter Burdette <peterburdette@gmail.com>
2022-01-05 10:10:20 -0500

    passed data with event and property binding up to parent component and down to child component

--- 05-components-and-databinding-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ 05-components-and-databinding-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,8 +8,15 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  // setting the EventEmitter type to <void> because it's not passing any information because it contains no information
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelected() {
+    // nothing is passed using emit() because of the <void> type
+    this.recipeSelected.emit();
+  }
 }
--- 05-components-and-databinding-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 05-components-and-databinding-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,6 +7,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  // <Recipe> is the type that is being passed
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   // the 'Recipe[]' indicates that we are using our model which contains the types of properties that will be used within this array.
   recipes: Recipe[] = [
     new Recipe(
@@ -16,8 +18,8 @@ export class RecipeListComponent implements OnInit {
       'https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale'
     ),
     new Recipe(
-      'Test Recipe',
-      'This is a test recipe',
+      'Another Test Recipe',
+      'This is another test recipe',
       'https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale'
     ),
   ];
@@ -25,4 +27,9 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+    // console.log(recipe);
+  }
 }
