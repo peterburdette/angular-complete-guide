87b4ae847671dae709332493dd866a954d2aa229
Peter Burdette <peterburdette@gmail.com>
2022-01-06 17:32:42 -0500

    made AccountsService globally accessible throughout the app

--- 08-services-and-dependency-injection/src/app/app.module.ts
+++ 08-services-and-dependency-injection/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { AccountComponent } from "./account/account.component";
 import { NewAccountComponent } from "./new-account/new-account.component";
+import { AccountsService } from "./accounts.service";
 
 @NgModule({
   declarations: [AppComponent, AccountComponent, NewAccountComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [AccountsService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
