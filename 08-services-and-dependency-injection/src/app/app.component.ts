b5039d1aff931635187a62a5ff5e49ef8bb1ff80
Peter Burdette <peterburdette@gmail.com>
2022-01-06 17:31:16 -0500

    updated note on services (specifically hierarchical injectors)

--- 08-services-and-dependency-injection/src/app/app.component.ts
+++ 08-services-and-dependency-injection/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { AccountsService } from "./accounts.service";
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [AccountsService], // because of the trickle down effect, AccountsService needs to be provided at the top level component
+  providers: [AccountsService], // The Angular dependency injector is a hierarchical injector. That means that if we provide a service in some place of our app the Angular framework knows how to create an instance of that service for the top level component and all child components. Essentially this means that if the service is defined at the highest level, all child components will receive the same instance of that service definition.
 })
 export class AppComponent implements OnInit {
   accounts: { name: string; status: string }[] = [];
