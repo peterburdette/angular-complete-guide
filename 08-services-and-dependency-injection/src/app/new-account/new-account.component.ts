dc5f6e7548a7d6720dc8680f21c6baa7aadf878d
Peter Burdette <peterburdette@gmail.com>
2022-01-06 17:27:26 -0500

    created a new data service and injected it into child components

--- 08-services-and-dependency-injection/src/app/new-account/new-account.component.ts
+++ 08-services-and-dependency-injection/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
+import { AccountsService } from "../accounts.service";
 import { LoggingService } from "../logging.service";
 
 @Component({
@@ -8,16 +9,15 @@ import { LoggingService } from "../logging.service";
   providers: [LoggingService], // with this added Angular will recognize, when analyzing the component, that it should provide a 'loggingService' and it will set itself up to do so
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
-
   // this informs Angular that we will need an instance of the LoggingService
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus,
-    });
+    this.accountsService.addAccount(accountName, accountStatus);
+
     // using a service by injecting the service here
     this.loggingService.logStatusChange(accountStatus);
     // console.log("A server status changed, new status: " + accountStatus);
