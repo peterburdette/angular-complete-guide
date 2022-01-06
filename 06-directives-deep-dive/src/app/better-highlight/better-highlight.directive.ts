e80f7fb47a0ae85a49d6fde74b569a13acf631ce
Peter Burdette <peterburdette@gmail.com>
2022-01-06 09:32:39 -0500

    utilized @HostListener() to listen to events that happen in the DOM

--- 06-directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
+++ 06-directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from "@angular/core";
 
 // This directive is best practice
 // Now why is it a better approach? Angular is not limited to running in the browser here, it for example also works with service workers and these are environments where you might not have access to the DOM.
@@ -10,10 +16,23 @@ export class BetterHighlightDirective implements OnInit {
 
   ngOnInit(): void {
     // setStyle() takes two args. The first is the targeted element, the second is the css property style we want to use and the third arg is the value that is to be assigned to the property. The fourth arg is optional and it is a flag (e.g. if you want to set '!important')
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", "blue");
+  }
+
+  // @HostListener directive that takes one argument as a string and works in tandem with a method that will be executed. 'Mouseenter' and 'mouseleave' were chosen as args because they are events that are supported by DOM elements.
+  @HostListener("mouseenter") mouseover(eventData: Event) {
     this.renderer.setStyle(
       this.elRef.nativeElement,
       "background-color",
       "blue"
+    );
+  }
+
+  @HostListener("mouseleave") mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      "background-color",
+      "transparent"
     );
   }
 }
