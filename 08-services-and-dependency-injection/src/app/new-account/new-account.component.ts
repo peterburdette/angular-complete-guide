2f6ec99da306914253cbfe4766ee8546b798985c
Peter Burdette <peterburdette@gmail.com>
2022-01-06 16:29:00 -0500

    utilized the newly created service by injecting it into components

--- 08-services-and-dependency-injection/src/app/new-account/new-account.component.ts
+++ 08-services-and-dependency-injection/src/app/new-account/new-account.component.ts
@@ -1,18 +1,25 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { LoggingService } from "../logging.service";
 
 @Component({
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
+  providers: [LoggingService], // with this added Angular will recognize, when analyzing the component, that it should provide a 'loggingService' and it will set itself up to do so
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
+
+  // this informs Angular that we will need an instance of the LoggingService
+  constructor(private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus,
     });
-    console.log("A server status changed, new status: " + accountStatus);
+    // using a service by injecting the service here
+    this.loggingService.logStatusChange(accountStatus);
+    // console.log("A server status changed, new status: " + accountStatus);
   }
 }
