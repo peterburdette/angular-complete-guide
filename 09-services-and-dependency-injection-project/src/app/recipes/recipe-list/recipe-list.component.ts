4ae88079384e8f3cc97c114dad5dd29b64512bc9
Peter Burdette <peterburdette@gmail.com>
2022-01-10 09:30:54 -0500

    finished and implemented the recipe service

--- 09-services-and-dependency-injection-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 09-services-and-dependency-injection-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,24 +10,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   // <Recipe> is the type that is being passed
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes = [];
 
-  // the 'Recipe[]' indicates that we are using our model which contains the types of properties that will be used within this array.
-  recipes: Recipe[] = [
-    new Recipe(
-      'Test Recipe',
-      'This is a test recipe',
-      'https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale'
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is another test recipe',
-      'https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale'
-    ),
-  ];
+  constructor(private recipeService: RecipeService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- 09-services-and-dependency-injection-project/src/app/services/recipe.service.ts
+++ 09-services-and-dependency-injection-project/src/app/services/recipe.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
-export class RecipeService {}
+export class RecipeService {
+  // the 'Recipe[]' indicates that we are using our model which contains the types of properties that will be used within this array.
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is another test recipe',
+      'https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale'
+    ),
+  ];
+
+  getRecipes() {
+    // 'slice()' will cause this method to return a copy of the recipies array
+    return this.recipes.slice();
+  }
+}
