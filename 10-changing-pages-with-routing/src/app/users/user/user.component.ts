c8b0e96d25dccab8702279744c4f03b09e6ca523
Peter Burdette <peterburdette@gmail.com>
2022-01-11 15:39:09 -0500

    learned about ngOnDestroy() and unsubscribe() when using route observables

--- 10-changing-pages-with-routing/src/app/users/user/user.component.ts
+++ 10-changing-pages-with-routing/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
+  paramsSubscription: Subscription;
 
   // the ActivatedRoute object that is injected will give access to the id passed into the URL => selected user
   constructor(private route: ActivatedRoute) {}
@@ -23,9 +25,14 @@ export class UserComponent implements OnInit {
 
     // an observable (in this case it is 'params') is an easy way to subscribe to some event which might happen in the future, to then exectue some code when it happens without having to wait for it now
     // the subscribe() method will fire whenever new data is sent through the observable (aka whenever the parameters change in this use case)
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.user.id = params["id"];
       this.user.name = params["name"];
     });
+  }
+
+  // this part does not necessarily have to be implemented since Angular cleans up the subscription in the background when this component is destroyed - this is something that you will have to manually unsubscribe() from if you use your own observables
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 }
