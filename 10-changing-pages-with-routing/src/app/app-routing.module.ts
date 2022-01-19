88bb33b3cbb3a260376982c7c38bd501d78627de
Peter Burdette <peterburdette@gmail.com>
2022-01-19 09:31:44 -0500

    protected child (nested) routes with canActivateChild

--- 10-changing-pages-with-routing/src/app/app-routing.module.ts
+++ 10-changing-pages-with-routing/src/app/app-routing.module.ts
@@ -23,7 +23,8 @@ const appRoutes: Routes = [
   },
   {
     path: "servers",
-    canActivate: [AuthGuardService],
+    // canActivate: [AuthGuardService],
+    canActivateChild: [AuthGuardService],
     component: ServersComponent,
     children: [
       { path: ":id", component: ServerComponent },
--- 10-changing-pages-with-routing/src/app/auth-guard.service.ts
+++ 10-changing-pages-with-routing/src/app/auth-guard.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   Router,
   RouterStateSnapshot,
+  UrlTree,
 } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
 
   // Interface that a class can implement to be a guard deciding if a route can be activated. If all guards return true, navigation continues. If any guard returns false, navigation is cancelled.
@@ -24,5 +26,14 @@ export class AuthGuardService implements CanActivate {
         this.router.navigate(["/"]);
       }
     });
+  }
+
+  // Interface that a class can implement to be a guard deciding if a child route can be activated. If all guards return true, navigation continues. If any guard returns false, navigation is cancelled.
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    // The CanActivateChild method takes the same form as the canActivate method, so it has the route and state and it returns an observable, promise or boolean. Since this is exactly the same form and we want to run the same logic, we can simply return this canActivate() and pass our route and state as arguments since canActivate expects these two arguments.
+    return this.canActivate(route, state);
   }
 }
