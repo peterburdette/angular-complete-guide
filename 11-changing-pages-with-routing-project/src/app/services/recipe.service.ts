a535f5c78fa9388ecc562575f1fdfff72df64723
Peter Burdette <peterburdette@gmail.com>
2022-01-20 14:43:30 -0500

    passed dynamic parameters to the service using links and updated Recipe model

--- 11-changing-pages-with-routing-project/src/app/services/recipe.service.ts
+++ 11-changing-pages-with-routing-project/src/app/services/recipe.service.ts
@@ -9,12 +9,14 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
+      0,
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
+      1,
       'Big Fat Burger',
       'What else you need to say?',
       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
