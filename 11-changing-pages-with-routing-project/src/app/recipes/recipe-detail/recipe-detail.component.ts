fa3546de913721da841c43f6c0b259962d57cae5
Peter Burdette <peterburdette@gmail.com>
2022-01-20 15:43:51 -0500

    retrieved route parameters and added the ability to navigate to the edit page for new and existing recipes

--- 11-changing-pages-with-routing-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ 11-changing-pages-with-routing-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { Recipe } from '../recipe.model';
 
@@ -14,7 +14,8 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -31,5 +32,9 @@ export class RecipeDetailComponent implements OnInit {
     console.log(this.recipe.ingredients);
     console.log('working');
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
