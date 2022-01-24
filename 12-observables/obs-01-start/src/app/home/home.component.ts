376a23643be77ce239a9afa501b1c4f625ce9a4e
Peter Burdette <peterburdette@gmail.com>
2022-01-24 16:48:20 -0500

    learned about how observables work (subscribing/unsubscribing)

--- 12-observables/obs-01-start/src/app/home/home.component.ts
+++ 12-observables/obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+
+import { interval, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  // created a property that will be used to store the interval subscription (type is 'Subscription') so that onDestroy can be implemented to unsubscribe from the current subscription
+  private firstObservableSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // 'interval' is the observable that is imported from rxjs
+    // Observables don't necesarily stop emitting values just because we navigate away from a page. There are certain observables that emit a value once and then they're done, like for example an HTTP request where you get back a response. There are other observables that keep on emitting values and need to be stopped to prevent memory leaks.
+    this.firstObservableSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy(): void {
+    // unsubscribes from the active interval subscription
+    this.firstObservableSubscription.unsubscribe();
+  }
 }
--- 12-observables/obs-01-start/src/app/user/user.component.ts
+++ 12-observables/obs-01-start/src/app/user/user.component.ts
@@ -13,6 +13,7 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
     // the 'params' in 'this.route' is an observable - it's an observable that is being subscribed to. Observables are constructs to which you subscribe to be informed about the changes in data (params is a stream of route parameters).
+    // Unsubscribe is not needed for this observable ('params') because Angular automatically unsubscribes for us.
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
