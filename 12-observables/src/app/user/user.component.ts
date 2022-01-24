127e42824d50aeb53bc989720656342291d6d726
Peter Burdette <peterburdette@gmail.com>
2022-01-24 11:45:18 -0500

    added notes

--- 12-observables/src/app/user/user.component.ts
+++ 12-observables/src/app/user/user.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    // the 'params' in 'this.route' is an observable - it's an observable that is being subscribed to. Observables are constructs to which you subscribe to be informed about the changes in data (params is a stream of route parameters).
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+    console.log(this.route.params);
+    console.log("this.id: " + this.id);
   }
 }
