3666c512f2f7a9981f93168e203b6fb13d6071e1
Peter Burdette <peterburdette@gmail.com>
2022-01-25 10:54:00 -0500

    created a custom observable

--- 12-observables/obs-02-first-custom-observable/src/app/home/home.component.ts
+++ 12-observables/obs-02-first-custom-observable/src/app/home/home.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
   private firstObsSubscription: Subscription;
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    // the 'observer' is the part that is interested in being informed about new data, about errors or about the observable being completed.
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+      console.log(data);
     });
   }
 
   ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();
   }
-
 }
