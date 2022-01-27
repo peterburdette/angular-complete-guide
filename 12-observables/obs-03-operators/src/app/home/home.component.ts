2d0ed9d164f62bacb7b52e1aaf7fd889a009131e
Peter Burdette <peterburdette@gmail.com>
2022-01-27 10:42:18 -0500

    implemented operators map and filter within the pipe() method

--- 12-observables/obs-03-operators/src/app/home/home.component.ts
+++ 12-observables/obs-03-operators/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 
 import { interval, Subscription, Observable } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
@@ -41,18 +42,32 @@ export class HomeComponent implements OnInit, OnDestroy {
     // 1 - observable execution handling
     // 2 - error handling
     // 3 - completion handling
-    this.firstObsSubscription = customIntervalObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-        alert(error);
-      },
-      () => {
-        console.log("Completed!");
-      }
-    );
+    // Operators can be used on any observable. Every observable has a pipe method. The observable with the pipe method needs to be subscribed to otherwise the data format within the observable will only show the basic subscription output.
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data) => {
+          // return true if data is greater than 0
+          return data > 0;
+        }),
+
+        // So this is the map operator and again just as before, this is kind of a constructed example here, it is of course more useful for example if you're fetching complex data from a web server and you want to transform that data before you use it in a component.
+        map((data: number) => {
+          return "Round: " + (data + 1);
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error);
+        },
+        () => {
+          console.log("Completed!");
+          alert("Completed!");
+        }
+      );
   }
 
   ngOnDestroy(): void {
