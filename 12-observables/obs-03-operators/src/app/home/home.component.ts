f1910a464635d10d6d4d86bebe4975fbccbcd30a
Peter Burdette <peterburdette@gmail.com>
2022-01-27 10:01:00 -0500

    reverted finished project to starting template

--- 12-observables/obs-03-operators/src/app/home/home.component.ts
+++ 12-observables/obs-03-operators/src/app/home/home.component.ts
@@ -1,55 +1,61 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
-import { interval, Subscription, Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { interval, Subscription, Observable } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
   private firstObsSubscription: Subscription;
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
     // this.firstObsSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // });
-    const customIntervalObservable = Observable.create(observer => {
+
+    // the 'observer' is the part that is interested in being informed about new data, about errors or about the observable being completed.
+    const customIntervalObservable = Observable.create((observer) => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if (count === 5) {
+
+        if (count === 3) {
+          // when complete() is called the observable will come to a halt
           observer.complete();
         }
+
         if (count > 3) {
-          observer.error(new Error('Count is greater 3!'));
+          // throwing an error will cancel the observable, it will not complete it
+          observer.error(new Error("Counter is greater than 3!"));
         }
+
         count++;
       }, 1000);
     });
 
-
-    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
-      return data > 0;
-    }), map((data: number) => {
-      return 'Round: ' + (data + 1);
-    })).subscribe(data => {
-      console.log(data);
-    }, error => {
-      console.log(error);
-      alert(error.message);
-    }, () => {
-      console.log('Completed!');
-    });
+    // subscribe takes three arguments:
+    // 1 - observable execution handling
+    // 2 - error handling
+    // 3 - completion handling
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error);
+      },
+      () => {
+        console.log("Completed!");
+      }
+    );
   }
 
   ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();
   }
-
 }
