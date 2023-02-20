73d56f9417fd0dc30fde56104726c8515c09a2e4
Peter Burdette <peterburdette@gmail.com>
2023-02-20 14:47:28 -0500

    added a custom validator

--- 14-handling-forms/forms-reactive-start/src/app/app.component.ts
+++ 14-handling-forms/forms-reactive-start/src/app/app.component.ts
@@ -9,14 +9,17 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit() {
     // an object is passed into the new FormGroup - the object can contain controls which are just key/value pairs that are passed via the object to the new FormGroup
   
     // new FormControl is a constructor where arguments get passed in - first arg is the initial state, second arg is a single validator or array of validators that are to be applied to this control, the third arg is a potential asynchronous validator(s)
+
+    // need to bind 'this' to the custom validator (forbiddenNames) because this it is not being called from inside the class
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -35,5 +38,14 @@ export class AppComponent implements OnInit {
 
   getControls() {
     return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  // custom validator
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    // checks to see if the value of the control is part of the forbiddenUsernames array, if true then return an object that says the name is forbidden
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
